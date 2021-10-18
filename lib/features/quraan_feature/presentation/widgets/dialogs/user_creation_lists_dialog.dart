79efda9cdc0755025a531d85f3d6e2ca3d28eb7c
fabuthaher <farah.abuthaher@gmail.com>
2021-10-18 10:28:26 +0300

    Tafsir applicationTafsir application

--- lib/features/quraan_feature/presentation/widgets/dialogs/user_creation_lists_dialog.dart
+++ lib/features/quraan_feature/presentation/widgets/dialogs/user_creation_lists_dialog.dart
@@ -19,17 +19,84 @@ class UserCreayedListsDialog extends StatelessWidget {
               Tab(text: 'قوائم الاّيات'),
             ],
           ),
-          body: TextButton(
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Text(
-                  'إنشاء قائمة',
-                  style: TextStyle(color: Colors.blue),
-                ),
+          body: TabBarView(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: TextButton(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(
+                        'إنشاء قائمة',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    onPressed: () => customDialog(context,ListCreate('قوائم البحث'))),
               ),
-              onPressed: () {}),
+              Align(
+                alignment: Alignment.topLeft,
+                child: TextButton(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(
+                        'إنشاء قائمة',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    onPressed: () => customDialog(context,ListCreate('قوائم الاّيات'))),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+customDialog(BuildContext context, Widget dialogCustom) {
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return dialogCustom;
+    },
+  );}
+class ListCreate extends StatelessWidget {
+String title;
+
+ListCreate(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      titlePadding: EdgeInsets.zero,
+      title: Container(
+          height: 50,
+          color: Color.fromARGB(255, 233, 239, 234),
+          child: Text(
+           title,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black),
+          )),
+      content: Container(
+        height: 200,
+        child: Column(
+          children: [
+            Text('اسم القائمة'),
+            SizedBox(width: 100,child: TextField(textAlign: TextAlign.right,),),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [TextButton(onPressed: null, child: Text('الغاء',style: TextStyle(fontSize: 15, color: Colors.blue),)),
+                TextButton(onPressed: null, child: Text('حفظ',style: TextStyle(fontSize: 15, color: Colors.blue),)),
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
+
